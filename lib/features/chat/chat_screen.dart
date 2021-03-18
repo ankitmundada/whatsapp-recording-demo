78a6e9ede7e991bc114fc1d05ff62cba8ccde8f0
Ankit Mundada <aomundada@gmail.com>
2021-03-18 15:33:11 +0530

    Add audio recorder and audio player blocs to chat screen

--- lib/features/chat/chat_screen.dart
+++ lib/features/chat/chat_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:whatsappaudio/commons/blocs/audio_player/audio_player_bloc.dart';
+import 'package:whatsappaudio/commons/blocs/audio_recorder/audio_recorder_bloc.dart';
 import 'package:whatsappaudio/features/chat/widgets/recording_controller_widget.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -6,21 +9,55 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AudioPlayerBloc()),
+        BlocProvider(create: (context) => AudioRecorderBloc()),
+      ],
+      child: ChatScreenWidget(),
+    );
+  }
+}
+
+class ChatScreenWidget extends StatefulWidget {
+  ChatScreenWidget({Key key}) : super(key: key);
+
+  @override
+  _ChatScreenWidgetState createState() => _ChatScreenWidgetState();
+}
+
+class _ChatScreenWidgetState extends State<ChatScreenWidget> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Whatsapp Recorder Demo'),
-      ),
-      body: LayoutBuilder(
+      appBar: AppBar(title: const Text('Whatsapp Recorder Demo')),
+      body: const AudioRecorderWidget(),
+    );
+  }
+}
+
+class AudioRecorderWidget extends StatelessWidget {
+  final double height;
+  const AudioRecorderWidget({
+    Key key,
+    this.height = 64,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      child: LayoutBuilder(
         builder: (context, constraints) => Padding(
           padding: const EdgeInsets.all(8.0),
           child: RecordingControllerWidget(
             maxWidth: constraints.maxWidth,
             maxHeight: constraints.maxHeight,
             onRecordingDone: () {
-              displaySnackbar(context, 'Recording Finished!');
+              _displaySnackbar(context, 'Recording Finished!');
             },
             onCancel: () {
-              displaySnackbar(context, 'Recording Cancelled.');
+              _displaySnackbar(context, 'Recording Cancelled.');
             },
           ),
         ),
@@ -28,7 +65,7 @@ class ChatScreen extends StatelessWidget {
     );
   }
 
-  displaySnackbar(BuildContext context, String message) {
+  _displaySnackbar(BuildContext context, String message) {
     ScaffoldMessenger.of(context)
       ..hideCurrentSnackBar()
       ..showSnackBar(SnackBar(content: Text(message)));
